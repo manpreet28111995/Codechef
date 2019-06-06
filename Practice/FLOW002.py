7533d1d90fc00e9b3627f207cf3b0c85c3c2f6e8
Manpreet Singh <manpreet28111995@users.noreply.github.com>
2019-06-06 16:56:12 +0530

    FLOW002

---
+++ Practice/FLOW002.py
@@ -0,0 +1,10 @@
+#Problem Link: https://www.codechef.com/problems/FLOW002
+#Author: Manpreet Singh
+try:
+    for i in range(int(input())):
+        n,k=input().split()
+        n=int(n)
+        k=int(k)
+        print(n%k)
+except:
+    pass
