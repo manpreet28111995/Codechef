98a17e7afd4988f82701ca76c061b8f8c6ef381e
Manpreet Singh <manpreet28111995@users.noreply.github.com>
2019-06-06 17:26:42 +0530

    FSQRT

---
+++ Practice/FSQRT.py
@@ -0,0 +1,8 @@
+#Problem Link: https://www.codechef.com/problems/FSQRT
+import math
+try:
+    for i in range(int(input())):
+        n=int(input())
+        print(int(math.sqrt(n)))        
+except:
+    pass
