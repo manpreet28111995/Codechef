d0a77d8d955a0ce304a79c16108aad4d60bdf4ae
Manpreet Singh <manpreet28111995@users.noreply.github.com>
2019-06-06 15:45:54 +0530

    FLOW001

---
+++ Practice/FLOW001.py
@@ -0,0 +1,10 @@
+#Problem Link: https://www.codechef.com/problems/FLOW001
+#Author:Manpreet Singh
+try:
+    for i in range(int(input())):
+        a,b=input().split()
+        a=int(a)
+        b=int(b)
+        print(a+b)
+except:
+    pass
