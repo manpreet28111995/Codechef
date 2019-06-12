145ce1d92f26c53bf1f0d075a1fd8b4e14012fa3
Manpreet Singh <manpreet28111995@users.noreply.github.com>
2019-06-12 13:04:31 +0530

    FLOW004

---
+++ Beginner/FLOW004.py
@@ -0,0 +1,8 @@
+#Problem Link: https://www.codechef.com/problems/FLOW004
+try:
+    for i in range(int(input())):
+        string=input()
+        ans=int(string[-1:])+int(string[:1])
+        print(ans)
+except:
+    pass
