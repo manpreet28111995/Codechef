f4b910d57f97979e90f7c0b5c1e3e5fa9fb19633
Manpreet Singh <manpreet28111995@users.noreply.github.com>
2019-06-13 20:52:39 +0530

    FLOW018

---
+++ Beginner/FLOW018.py
@@ -0,0 +1,10 @@
+#Problem Link : https://www.codechef.com/problems/FLOW018/
+try:
+    for i in range(int(input())):
+        a=int(input())
+        prod=1
+        for x in range(1,a+1):
+            prod=prod*x
+        print(prod)
+except:
+    pass
