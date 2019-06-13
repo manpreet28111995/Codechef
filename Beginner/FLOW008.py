da44cee7f1197de434dfd56377c4543275899594
Manpreet Singh <manpreet28111995@users.noreply.github.com>
2019-06-13 20:02:29 +0530

    FLOW008

---
+++ Beginner/FLOW008.py
@@ -0,0 +1,10 @@
+#Problem Link : https://www.codechef.com/problems/FLOW00/
+try:
+    for i in range(int(input())):
+        s=int(input())
+        if s<10:
+            print ('What an obedient servant you are!')
+        else:
+            print('-1')
+except:
+    pass
